4f6d00671d47fad079fc5fe5989962effd6933bd
Oleksandr Lokhonia <olokhonia@opengov.com>
2023-02-03 22:00:44 +0200

    update ArticlePage to use Unvote, CommentList and Add Comment From components

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
+import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = () => {
     const { name } = useParams();
@@ -17,17 +20,20 @@ const ArticlePage = () => {
         };
         
         fetchData();
-    }, [name]);
+    }, [name, articleInfo]);
 
     if (!article) return (<NotFoundPage />);
 
     return (
         <>
         <h1>{ article.title }</h1>
-        <p>This post has been upvoted { articleInfo.upvotes } times</p>
+        <UpvotesSection articleName={ name } upvotes={ articleInfo.upvotes } setArticleInfo={ setArticleInfo } />
+
         { 
             article.content.map((paragraph, key) => (<p key={ key }>{ paragraph }</p>))
         }
+        <CommentsList comments={ articleInfo.comments } />
+        <AddCommentForm articleName={ name } setArticleInfo={ setArticleInfo } />
         </>
     );
 }
